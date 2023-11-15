149e4a204f0eb3beb1b00756713f0499da4e76c8
Oleg Cherr <olegcherr@gmail.com>
2023-11-15 19:44:23 +0100

    Add mixins.scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    include: '**/*.svg',
-    exclude: '**/*.svg?url'
-  })],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "./src/mixins.scss";',
+			},
+		},
+	},
+	plugins: [
+		react(),
+		svgr({
+			include: '**/*.svg',
+			exclude: '**/*.svg?url',
+		}),
+	],
 })
