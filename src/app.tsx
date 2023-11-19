20323c64f5ad57a768ccb2fe10234a9f71e39f27
Oleg Cherr <olegcherr@gmail.com>
2023-11-19 10:40:40 +0100

    Move QueryClientProvider to App

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { AuctionPage } from './pages/auctionPage/auctionPage.tsx'
@@ -6,16 +7,20 @@ import { DashboardPage } from './pages/dashboardPage/dashboardPage.tsx'
 import { RoutePath } from './routePath.ts'
 import { Web3ModalManager } from './utils/walletconnect.tsx'
 
+const queryClient = new QueryClient()
+
 export function App() {
 	return (
-		<Web3ModalManager>
-			<BrowserRouter>
-				<Routes>
-					<Route path={RoutePath.ROOT} element={<BrowsePage />} />
-					<Route path={RoutePath.AUCTION} element={<AuctionPage />} />
-					<Route path={RoutePath.DASHBOARD} element={<DashboardPage />} />
-				</Routes>
-			</BrowserRouter>
-		</Web3ModalManager>
+		<QueryClientProvider client={queryClient}>
+			<Web3ModalManager>
+				<BrowserRouter>
+					<Routes>
+						<Route path={RoutePath.ROOT} element={<BrowsePage />} />
+						<Route path={RoutePath.AUCTION} element={<AuctionPage />} />
+						<Route path={RoutePath.DASHBOARD} element={<DashboardPage />} />
+					</Routes>
+				</BrowserRouter>
+			</Web3ModalManager>
+		</QueryClientProvider>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './app.tsx'
 import 'normalize.css'
 import 'minireset.css'
 import './main.scss'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { App } from './app.tsx'
 import { invariant } from './utils/assert.ts'
 
 const root = document.getElementById('root')
 invariant(root)
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
+		<App />
 	</React.StrictMode>,
 )
